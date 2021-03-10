97febbf9826cecdb6586e93e282f0f78bbb02103
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2021-03-10 23:53:43 +0100

    small fix

--- editor/components/add-content.mjs
+++ editor/components/add-content.mjs
@@ -80,7 +80,7 @@ export default {
       color: '#F7FAFC',
       font: 'Rubik',
       spacing: '-0.04em',
-      colors: ['#1FBC9C', '#1CA085', '#2ECC70', '#27AF60', '#3398DB', '#2980B9', '#575fcf', '#3D556E', '#222F3D', '#ffdd59', '#F2C511', '#F39C19', '#E84B3C', '#C0382B', '#BDC3C8', '#DDE6E8', '#F7FAFC', '#FFFFFF']
+      colors: ['#1CA085', '#27AF60', '#1FBC9C', '#2ECC70', '#3398DB', '#2980B9', '#575fcf', '#3D556E', '#222F3D', '#ffdd59', '#F2C511', '#F39C19', '#E84B3C', '#C0382B', '#BDC3C8', '#DDE6E8', '#F7FAFC', '#FFFFFF']
     }
   },
 
