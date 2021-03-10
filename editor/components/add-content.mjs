b7ba6b737b610d9ea693139d576df565a13ca8c0
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2021-03-10 23:50:24 +0100

    Color contrast is now automatically calculated

--- editor/components/add-content.mjs
+++ editor/components/add-content.mjs
@@ -29,35 +29,18 @@ const template = `
 
 
 <div class="input-group mb-3">
-  <input class="form-control" v-model="color" @change="setColor(color, '#000')">
+  <input class="form-control" v-model="color" @change="setColor(color)">
   <div class="input-group-append">
     <button class="btn btn-outline-secondary w-100px" type="button" @click="choose=!choose"><span v-if="!choose">Choose</span><span v-if="choose">Close</span></button>
   </div>
 </div>
 
 <div v-if="choose">
-      <div class="swatch" style="background-color: #1FBC9C" @click="setColor('#1FBC9C', '#FFF')"></div>
-      <div class="swatch" style="background-color: #1CA085" @click="setColor('#1CA085', '#FFF')"></div>
-      <div class="swatch" style="background-color: #2ECC70" @click="setColor('#2ECC70', '#FFF')"></div>
-      <div class="swatch" style="background-color: #27AF60" @click="setColor('#27AF60', '#FFF')"></div>
 
-      <div class="swatch" style="background-color: #3398DB" @click="setColor('#3398DB', '#FFF')"></div>
-      <div class="swatch" style="background-color: #2980B9" @click="setColor('#2980B9', '#FFF')"></div>
-      <div class="swatch" style="background-color: #2980B9" @click="setColor('#2980B9', '#FFF')"></div>
-      <div class="swatch" style="background-color: #8E43AD" @click="setColor('#8E43AD', '#FFF')"></div>
+  <span v-for="mycolor in colors">
+      <div class="swatch" :style="'background-color: '+mycolor" @click="setColor(mycolor)"><i class="fas fa-check fa-center" v-if="color == mycolor"></i></div>
+  </span>
 
-      <div class="swatch" style="background-color: #3D556E" @click="setColor('#3D556E', '#FFF')"></div>
-      <div class="swatch" style="background-color: #222F3D" @click="setColor('#222F3D', '#FFF')"></div>
-      <div class="swatch" style="background-color: #F2C511" @click="setColor('#F2C511', '#000')"></div>
-      <div class="swatch" style="background-color: #F39C19" @click="setColor('#F39C19', '#FFF')"></div>
-
-      <div class="swatch" style="background-color: #E84B3C" @click="setColor('#E84B3C', '#FFF')"></div>
-      <div class="swatch" style="background-color: #C0382B" @click="setColor('#C0382B', '#FFF')"></div>
-      <div class="swatch" style="background-color: #DDE6E8" @click="setColor('#DDE6E8', '#000')"></div>
-      <div class="swatch" style="background-color: #BDC3C8" @click="setColor('#BDC3C8', '#FFF')"></div>
-
-      <div class="swatch" style="background-color: #F7FAFC" @click="setColor('#F7FAFC', '#000')"></div>
-      <div class="swatch" style="background-color: #FFFFFF; border: 1px solid #DDD;" @click="setColor('#FFFFFF', '#000')"></div>
 </div>
 
     <div class="label">Font</div>
@@ -94,9 +77,10 @@ export default {
       add: false,
       designer: false,
       choose: false,
-      color: '#1CA085',
+      color: '#F7FAFC',
       font: 'Rubik',
-      spacing: '-0.04em'
+      spacing: '-0.04em',
+      colors: ['#1FBC9C', '#1CA085', '#2ECC70', '#27AF60', '#3398DB', '#2980B9', '#575fcf', '#3D556E', '#222F3D', '#ffdd59', '#F2C511', '#F39C19', '#E84B3C', '#C0382B', '#BDC3C8', '#DDE6E8', '#F7FAFC', '#FFFFFF']
     }
   },
 
@@ -113,8 +97,9 @@ export default {
       this.entries.unshift(newItem);
       this.add = false;
     },
-    setColor: function(hex, hex2) {
+    setColor: function(hex) {
       this.color = hex;
+      let hex2 = colorContrast(hex);
       let root = document.documentElement;
       root.style.setProperty('--bg-color', hex);
       root.style.setProperty('--fg-color', hex2);
@@ -134,8 +119,38 @@ export default {
       document.getElementById("fonts").setAttribute("href", sheet);
     }
   },
-
   mounted() {
     console.log('Adder component mounted.')
+  }
+}
+
+function colorContrast(hex) {
+
+  var threshold = 150; /* about half of 256. Lower threshold equals more dark text on dark background  */
+  var hRed = hexToR(hex);
+  var hGreen = hexToG(hex);
+  var hBlue = hexToB(hex);
+
+  function hexToR(h) {
+    return parseInt((cutHex(h)).substring(0, 2), 16)
+  }
+
+  function hexToG(h) {
+    return parseInt((cutHex(h)).substring(2, 4), 16)
+  }
+
+  function hexToB(h) {
+    return parseInt((cutHex(h)).substring(4, 6), 16)
+  }
+
+  function cutHex(h) {
+    return (h.charAt(0) == "#") ? h.substring(1, 7) : h
+  }
+
+  var cBrightness = ((hRed * 299) + (hGreen * 587) + (hBlue * 114)) / 1000;
+  if (cBrightness > threshold) {
+    return "#000000";
+  } else {
+    return "#ffffff";
   }
 }
